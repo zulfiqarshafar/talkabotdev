dc472b14aec941d7d387b02d4e73b85438c9a2f1
Muhammad Zulfiqar Shafar <zul.shafar@gmail.com>
2019-01-15 18:22:15 +0700

    Updates

---
+++ precache-manifest.c6f88e5c0d6a8584eb49f2721525434a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/talkabotdev/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2a30fa2d6c2b29954f09",
+    "url": "/talkabotdev/static/js/runtime~main.2a30fa2d.js"
+  },
+  {
+    "revision": "4d14d8ae6e1cbc35b3fd",
+    "url": "/talkabotdev/static/js/main.4d14d8ae.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/talkabotdev/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "4d14d8ae6e1cbc35b3fd",
+    "url": "/talkabotdev/static/css/main.56e3d899.chunk.css"
+  },
+  {
+    "revision": "ce6f1ed7c3c586b9f9114f958e2207b2",
+    "url": "/talkabotdev/index.html"
+  }
+];
